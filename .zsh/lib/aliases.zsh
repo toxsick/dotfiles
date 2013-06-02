4d80eababfb852eb53766d1d4bbd926a2d5bfed1
hansotronic <serviceshansen@gmail.com>
2013-06-02 18:17:13 +0200

    add post aliases

--- .zsh/lib/aliases.zsh
+++ .zsh/lib/aliases.zsh
@@ -1,3 +1,8 @@
 alias ll='ls -la'
 alias lh='ls -lah'
 alias ...='cd ../..'
+
+# PXST
+alias pxstapi='cd ~/Coding/pxst-app/pxst-api/;fab'
+alias pxstdecider='cd ~/Coding/pxst-app/pxst-decider/;fab'
+alias pxstworker='cd ~/Coding/pxst-app/pxst-worker/;fab'
